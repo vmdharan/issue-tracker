a14482b2a529e1ec464435fdc6b7b81d6d97c1f9
vmdharan <vmdharan@users.noreply.github.com>
2024-03-20 22:28:28 +1100

    Update index.tsx
    Fixed error in ElementRoutes for Ticket Categories.

--- portal/src/routes/ElementRoutes/index.tsx
+++ portal/src/routes/ElementRoutes/index.tsx
@@ -1,15 +1,16 @@
 import { RouteObject } from 'react-router-dom';
-import { OrganisationElementRoute } from '../../schemas/organisation';
-import { ProductElementRoute } from '../../schemas/product';
-import { ProductCategoryElementRoute } from '../../schemas/product_category';
-import { TicketElementRoute } from '../../schemas/ticket';
-import { TicketSeverityElementRoute } from '../../schemas/ticket_severity';
-import { UserElementRoute } from '../../schemas/user';
+import { OrganisationElementRoute } from 'schemas/organisation';
+import { ProductElementRoute } from 'schemas/product';
+import { ProductCategoryElementRoute } from 'schemas/product_category';
+import { TicketElementRoute } from 'schemas/ticket';
+import { TicketSeverityElementRoute } from 'schemas/ticket_severity';
+import { TicketCategoryElementRoute } from 'schemas/ticket_category';
+import { UserElementRoute } from 'schemas/user';
 
 const ElementRoutes: RouteObject[] = [
     TicketElementRoute,
     TicketSeverityElementRoute,
-    TicketElementRoute,
+    TicketCategoryElementRoute,
     ProductElementRoute,
     ProductCategoryElementRoute,
     UserElementRoute,
